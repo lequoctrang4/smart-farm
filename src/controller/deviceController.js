079d5a554fcab2ac1993e338cc50f29aedb41541
lequoctrang4 <85050886+lequoctrang4@users.noreply.github.com>
2023-03-23 01:12:50 +0700

    23/3/1:12AM

--- src/controller/deviceController.js
+++ src/controller/deviceController.js
@@ -18,8 +18,12 @@ let editControlEquip = async (req, res) =>{
     let id = req.params.id;
     let body = req.body;
     let result = await deviceModel.editControlEquip([id, body]);
+    if (result === true)
     return res.status(200).json({
         data: "success"
+    });
+    return res.status(400).json({
+        data: result
     });
 };
 
@@ -34,33 +38,80 @@ let deleteControlEquip = async (req, res) =>{
     })
 };
 
-let getAllDataEquip = async (req, res) =>{
-    return res.status(200).json({
+let setStatusControlEquip = async (req, res) => {
+    let id = req.params.id;
+    let status = req.params.status;
+    let rs = await deviceModel.setStatusControlEquip([status, id]);
+    if (rs) return res.status(200).json({
         data: "success"
+    })
+    else return res.status(400).json({
+        data: rs
+    })
+}
+let setAutoControlEquip = async (req, res) => {
+    let id = req.params.id;
+    let auto = req.params.auto;
+    let rs = await deviceModel.setAutoControlEquip([auto, id]);
+    if (rs) return res.status(200).json({
+        data: "success"
+    })
+    else return res.status(400).json({
+        data: rs
+    })
+}
+let getAllDataEquip = async (req, res) =>{
+    let result = await deviceModel.getAllDataEquip();
+    return res.status(200).json({
+        data: result
     });
 };
 
 let addDataEquip = async (req, res) =>{
-    return res.status(200).json({
-        data: "success"
-    });
+    let result = await deviceModel.addDataEquip(req.body);
+    if (result === true) return res.status(200).json({
+            data: "success"
+        });
+    return res.status(400).json({ data: result})
 };
 
 let editDataEquip = async (req, res) =>{
+    let id = req.params.id;
+    let body = req.body;
+    let result = await deviceModel.editDataEquip([id, body]);
+    if (result === true)
     return res.status(200).json({
         data: "success"
+    });
+    return res.status(400).json({
+        data: result
     });
 };
 
 let deleteDataEquip = async (req, res) =>{
-    return res.status(200).json({
-        data: "success"
-    });
+    let rs = await deviceModel.deleteDataEquip(req.params.id);
+    if(rs === true)
+        return res.status(200).json({
+            data: "success"
+        });
+    else return res.status(400).json({
+        data: rs
+    })
 };
-
+let setStatusDataEquip = async (req, res) => {
+    let id = req.params.id;
+    let status = req.params.status;
+    let rs = await deviceModel.setStatusDataEquip([status, id]);
+    if (rs) return res.status(200).json({
+        data: "success"
+    })
+    else return res.status(400).json({
+        data: rs
+    })
+}
 module.exports = {
-    getAllControlEquip, addControlEquip, editControlEquip, deleteControlEquip,
-    getAllDataEquip, addDataEquip, editDataEquip, deleteDataEquip
+    getAllControlEquip, addControlEquip, editControlEquip, deleteControlEquip, setStatusControlEquip, setAutoControlEquip,
+    getAllDataEquip, addDataEquip, editDataEquip, deleteDataEquip, setStatusDataEquip
 }
 
 
--- src/models/deviceModel.js
+++ src/models/deviceModel.js
@@ -8,7 +8,7 @@ let getAllControlEquip = async () =>{
 let addControlEquip = async (page) =>{
     try {
         let {id, name, position,type, farm_id} = page;
-        await pool.execute(`INSERT INTO control_equipment(id, name, postition, type, farm_id) VALUES (?,?,?,?,?)`,
+        await pool.execute(`INSERT INTO control_equipment(id, name, position, type, farm_id) VALUES (?,?,?,?,?)`,
         [id, name, position, type, farm_id]);
         return true;
     } catch (error) {
@@ -34,25 +34,67 @@ let deleteControlEquip = async (page) =>{
         return error.sqlMessage;
     }
 };
-
+let setStatusControlEquip = async (page) =>{
+    try {
+        await pool.execute(`UPDATE control_equipment SET status = ? WHERE id = ?`, [page[0], page[1]]);
+        return true;
+    } catch (error) {
+        return error.sqlMessage;
+    }
+};
+let setAutoControlEquip = async (page) =>{
+    try {
+        await pool.execute(`UPDATE control_equipment SET auto = ? WHERE id = ?`, [page[0], page[1]]);
+        return true;
+    } catch (error) {
+        return error.sqlMessage;
+    }
+};
 
 let getAllDataEquip = async () =>{
-    
+    let [rs] = await pool.execute(`select * from data_equipment`);
+    return rs;
 };
 
-let addDataEquip = async () =>{
-    
+let addDataEquip = async (page) =>{
+    try {
+        let {id, name, position, min, max, time, type, farm_id} = page;
+        await pool.execute(`INSERT INTO data_equipment(id, name, position, min, max, time, type, farm_id) VALUES (?,?,?,?,?,?,?,?)`,
+        [id, name, position, min, max, time, type, farm_id]);
+        return true;
+    } catch (error) {
+        return error.sqlMessage;
+    }
 };
 
-let editDataEquip = async () =>{
-    
+let editDataEquip = async (page) =>{
+    try {
+        let {id, name, position, min, max, time, type, farm_id} = page[1];
+        await pool.execute(`UPDATE data_equipment SET id = ?, name =?, position = ?, min =?, max = ?, time = ?, type =?, farm_id =? where id = ?`,
+         [id, name, position, min, max, time, type, farm_id, page[0]])
+        return true;
+    } catch (error) {
+        return error.sqlMessage;
+    }
 };
 
-let deleteDataEquip = async () =>{
-
+let deleteDataEquip = async (page) =>{
+    try {
+        await pool.execute(`DELETE FROM data_equipment WHERE id = ?`, [page]);
+        return true;
+    } catch (error) {
+        return error.sqlMessage;
+    }
 };
-
+let setStatusDataEquip = async (page) =>{
+    try {
+        await pool.execute(`UPDATE data_equipment SET status = ? WHERE id = ?`, [page[0], page[1]]);
+        return true;
+    } catch (error) {
+        return error.sqlMessage;
+    }
+};
 module.exports = {
-    getAllControlEquip, addControlEquip, editControlEquip, deleteControlEquip,
-    getAllDataEquip, addDataEquip, editDataEquip, deleteDataEquip
+    getAllControlEquip, addControlEquip, editControlEquip, deleteControlEquip, setStatusControlEquip, setAutoControlEquip,
+    getAllDataEquip, addDataEquip, editDataEquip, deleteDataEquip, setStatusDataEquip
 }
