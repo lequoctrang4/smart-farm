edfdf42da53bb04b90b07411afd3e4e006e8b808
lequoctrang4 <85050886+lequoctrang4@users.noreply.github.com>
2023-03-24 14:39:00 +0700

    123

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -6,7 +6,7 @@ export const logIn = async(formValue) => {
       // make axios post request
       const res = await axios({
         method: "post",
-        url: `${process.env.HOSTBACKEND}/user/login`,
+        url: `http://localhost:${process.env.PORT}/user/login`,
         body: formValue,
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
       });
